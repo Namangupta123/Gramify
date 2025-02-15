be9edcdb9f8cdc91ee9ddbc5532126cb9b8b1c6b
NamanGupta123 <namangupta0330@gmail.com>
2025-02-15 18:53:09 +0530

    Updates

--- src/background.js
+++ src/background.js
@@ -1,15 +1,26 @@
 import {getSelectedText, injectCorrectedText} from './utils.js';
 
+const API_URL='https://gramify-one.vercel.app/fix-grammer';
+
 chrome.commands.onCommand.addListener(async(command)=>{
-    if(command==='fix-grammer'){
+    if(command === 'fix-grammar'){
         const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-        if (!tab.id) return;
+        if(!tab){
+            console.log('No active tab found');
+            return;
+        }
+        if (!tab.id){
+            console.log("No tabID")
+            return;
+        }
     
         const selectedText = await getSelectedText(tab.id);
-        if (!selectedText) return;
-    
+        if (!selectedText) {
+            console.log("No selected text")
+            return;
+        }
         try {
-          const response = await fetch('http://localhost:3000/fix-grammar', {
+          const response = await fetch(API_URL, {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
@@ -23,5 +34,8 @@ chrome.commands.onCommand.addListener(async(command)=>{
         catch (error) {
           console.error('Error fixing grammar:', error);
         }
+    }
+    else{
+        console.log("Error in command");
     }
 });
